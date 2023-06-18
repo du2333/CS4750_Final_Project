8dc59b6a09087d8699a8e1b33e47b6cb97a85d94
Zephyrrr <kuangdadu@gmail.com>
2023-06-17 19:15:51 -0700

    Created Basic Navigation and corresponding pages

---
+++ lib/contentPages/PlayListPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PlayListPage extends StatefulWidget {
+  const PlayListPage({super.key});
+
+  @override
+  State<PlayListPage> createState() => _PlayListPageState();
+}
+
+class _PlayListPageState extends State<PlayListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("It's playlist page"),
+      ),
+    );
+  }
+}
