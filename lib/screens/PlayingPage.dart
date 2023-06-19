cf4669eec533ac6a95fd4d3f28c2ef2e15dfed88
Zephyrrr <kuangdadu@gmail.com>
2023-06-18 21:16:14 -0700

    Added music control buttons

--- lib/screens/PlayingPage.dart
+++ lib/screens/PlayingPage.dart
@@ -39,7 +39,7 @@ class _PlayingPageState extends State<PlayingPage> {
   //根据播放器状态来渲染对应的按钮样式
   Widget _playerButton(PlayerState playerState) {
     //获取状态
-    final processingState = playerState?.processingState;
+    final processingState = playerState.processingState;
     //判断如果加载或缓冲则变成缓冲图标
     if (processingState == ProcessingState.loading ||
         processingState == ProcessingState.buffering) {
@@ -73,17 +73,93 @@ class _PlayingPageState extends State<PlayingPage> {
     }
   }
 
+  //context用来获取当前主题色
+  Widget _shuffleButton(bool isEnabled) {
+    return IconButton(
+      icon: isEnabled
+            ? Icon(Icons.shuffle_on_outlined)
+            : Icon(Icons.shuffle),
+      //改成异步方法，才能被StreamBuilder异步更新UI
+      onPressed: () async {
+        //获取相反的状态
+        final enable = !isEnabled;
+        //如果随机播放模式没有被激活，那么按下按钮就表示激活随机播放
+        if(enable) {
+          await _player.shuffle();
+        }
+        //按下按钮之后，设定成相反的状态
+        await _player.setShuffleModeEnabled(enable);
+      },
+    );
+  }
+
+  //TODO 实现功能
+  Widget _previousButton() {
+    return IconButton(
+      icon: Icon(Icons.skip_previous),
+      onPressed: () {},
+    );
+  }
+
+  //TODO 实现功能
+  Widget _nextButton() {
+    return IconButton(
+      icon: Icon(Icons.skip_next),
+      onPressed: () {},
+    );
+  }
+
+  //TODO 实现功能
+  Widget _loopButton(BuildContext context, LoopMode loopMode) {
+    return IconButton(
+      icon: Icon(Icons.repeat),
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: StreamBuilder<PlayerState>(
-        stream: _player.playerStateStream,
-        builder: (context, snapshot) {
-          final playerState = snapshot.data;
-          return _playerButton(playerState!);
-        },
-      )),
+      body: Row(
+          children: [
+            //随机播放按钮
+            StreamBuilder<bool>(
+              stream: _player.shuffleModeEnabledStream,
+              builder: (_, snapshot) {
+                return _shuffleButton(snapshot.data ?? false);// ??表示前面如果有值就取前面，否则取后面和 snapshot.data ? snapshot.data : false 一样
+              },
+            ),
+            //上一首按钮
+            StreamBuilder<SequenceState?>(
+              stream: _player.sequenceStateStream,
+              builder: (_, __) {
+                return _previousButton();
+              },
+            ),
+            //播放按钮
+            StreamBuilder<PlayerState>(
+              stream: _player.playerStateStream,
+              //Widget构建器
+              builder: (_, snapshot) {
+                final playerState = snapshot.data;
+                return _playerButton(playerState!);
+              },
+            ),
+            //下一首按钮
+            StreamBuilder<SequenceState?>(
+              stream: _player.sequenceStateStream,
+              builder: (_, __) {
+                return _nextButton();
+              },
+            ),
+            //循环播放按钮
+            StreamBuilder<LoopMode>(
+              stream: _player.loopModeStream,
+              builder: (context, snapshot) {
+                return _loopButton(context, snapshot.data ?? LoopMode.off);
+              },
+            )
+          ]),
     );
   }
 }
