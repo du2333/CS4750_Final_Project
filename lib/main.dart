2a4c91e9f72a0e62c8c75c029e17a133b85eb219
Zephyrrr <kuangdadu@gmail.com>
2023-06-27 21:23:40 -0700

    Integrated Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:cloudjams/screens/Splash.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
