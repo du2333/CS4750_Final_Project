f9858d1bfd2a443641a09feaf1968ddbd043e653
Zephyrrr <kuangdadu@gmail.com>
2023-06-30 17:41:19 -0700

    Updated the launcher icon

--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,4 +1,3 @@
-import 'package:cloudjams/screens/UserPage.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
